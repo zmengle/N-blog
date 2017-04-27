f454cadf910d140e6aa5dd27410629556fb8e80c
zmengle <z550254092@163.com>
2017-04-27 13:14:18 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
 	cookieSecret:'myblog',
-	url:'mongodb://<dbuser>:<dbpassword>@ds143990.mlab.com:43990/blog'	//need you to change <>
+	url:'mongodb://root:zmengle155177@ds143990.mlab.com:43990/blog'	//need you to change <>
 	/*db:'blog',
 	host:'localhost',
 	port:'27017'*/
